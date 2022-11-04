421cb9041956b12e2c5cec0d60b0905c9b41d689
bazjerc <blaz.jerenec@gmail.com>
2022-11-04 20:43:57 +0100

    refactor modal component to use ReactDom.createPortal method and render it as a child of modal-root element

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,25 +1,58 @@
-import ModalBackground from "./ModalBackground";
-import "./Modal.css";
+import { useRef } from "react";
+import ReactDOM from "react-dom";
+import "./ModalOverlay.css";
+import "./ModalBackground.css";
+
+function ModalBackdrop(props) {
+  const modalBackgroundRef = useRef();
+
+  return (
+    <div
+      className="modal-background"
+      ref={modalBackgroundRef}
+      onClick={(e) => {
+        e.target === modalBackgroundRef.current && props.onCloseModal();
+      }}
+    />
+  );
+}
+
+function ModalOverlay(props) {
+  return (
+    <div className="modal-overlay">
+      <div className="modal-overlay-header">
+        <p>{props.header}</p>
+      </div>
+      <div className="modal-overlay-body">
+        <p>{props.body}</p>
+        <button
+          className="modal-overlay-button"
+          type="button"
+          onClick={props.onCloseModal}
+        >
+          Okay
+        </button>
+      </div>
+    </div>
+  );
+}
 
 function Modal(props) {
   return (
-    <ModalBackground onCloseModal={props.onCloseModal}>
-      <div className="modal">
-        <div className="modal-header">
-          <p>{props.header}</p>
-        </div>
-        <div className="modal-body">
-          <p>{props.body}</p>
-          <button
-            className="modal-button"
-            type="button"
-            onClick={props.onCloseModal}
-          >
-            Okay
-          </button>
-        </div>
-      </div>
-    </ModalBackground>
+    <>
+      {ReactDOM.createPortal(
+        <ModalBackdrop onCloseModal={props.onCloseModal} />,
+        document.getElementById("modal-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          header={props.header}
+          body={props.body}
+          onCloseModal={props.onCloseModal}
+        />,
+        document.getElementById("modal-root")
+      )}
+    </>
   );
 }
 
